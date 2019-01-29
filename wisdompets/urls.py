ddc3bd060366867f334087480b21ebb0bc8d0967
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-01-29 06:46:17 +0500

    Learning Django

---
+++ wisdompets/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from adoptions import views
+
+
+urlpatterns = [
+    path(r'admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path(r'adoptions/<int:id>/', views.pet_detail, name='pet_detail'),
+
+]
